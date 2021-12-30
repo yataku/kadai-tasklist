ff17eafa73cbf2e3e1d5f184590342e50ae8b80e
takuto.yamada <yama.taku.to@gmail.com>
2021-12-30 16:37:57 +0000

    create tasklist

---
+++ resources/views/tasks/create.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>タスク新規作成ページ</h1>
+    
+    <div class="row">
+        <div class="col-6">
+            {!! Form::model($task, ['route' => 'tasks.store']) !!}
+            
+                <div class="form-group">
+                    {!! Form::label('content', 'タスク内容:') !!}
+                    {!! Form::text('content', null, ['class' => 'form-controll']) !!}
+                </div>
+                
+                {!! Form::submit('作成', ['class' => 'btn btn-primary']) !!}
+            {!! Form::close() !!}
+        </div>
+    </div>
+
+@endsection
